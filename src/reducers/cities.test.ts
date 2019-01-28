42596f74e71461c5e9df248aead4c5dd920f9854
Sid Vishnoi <sidvishnoi8@gmail.com>
2019-01-28 18:10:57 +0530

    Revert state back to nested as per review
    remove _SEARCH actions
    update reducers and fix tests

--- src/reducers/cities.test.ts
+++ src/reducers/cities.test.ts
@@ -1,75 +1,175 @@
-import { State } from '../state';
 import reducer, { ActionType } from './cities';
+import { State } from '../state';
 
 describe('Reducers - cities', () => {
   describe('defaults', () => {
-    const action = { type: 'ANY' } as ActionType;
-    expect(reducer(undefined, action)).toEqual([]);
+    const action = ({ type: 'ANY' } as unknown) as ActionType;
+    expect(reducer(undefined, action)).toEqual({ allIds: [], byId: {} });
   });
 
   describe('ADD_CITY', () => {
-    const action = { type: 'ADD_CITY', name: 'PASS' };
+    const action = { type: 'ADD_CITY', name: 'PASS' } as ActionType;
 
     it('does nothing if city is already in list', () => {
-      const inputState = ['PASS'];
+      const inputState = { allIds: ['PASS'], byId: {} };
       const outputState = reducer(inputState, action);
       expect(outputState).toEqual(inputState);
     });
 
     it('adds new city when list is empty', () => {
-      const inputState = [] as State['cities'];
+      const inputState = { allIds: [] as string[], byId: {} };
       const outputState = reducer(inputState, action);
-      expect(outputState).toEqual(['PASS']);
+      expect(outputState).toEqual({
+        allIds: ['PASS'],
+        byId: {
+          PASS: {
+            distance: { data: null, state: 'loading' },
+            places: { data: null, state: 'loading' },
+            weather: { data: null, state: 'loading' },
+          },
+        },
+      });
     });
 
     it('adds new city in start of list', () => {
-      const inputState = ['ONE', 'TWO'];
+      const inputState = { allIds: ['ONE', 'TWO'], byId: {} };
       const outputState = reducer(inputState, action);
-      expect(outputState).toEqual(['PASS', 'ONE', 'TWO']);
+      expect(outputState).toEqual({
+        allIds: ['PASS', 'ONE', 'TWO'],
+        byId: {
+          PASS: {
+            distance: { data: null, state: 'loading' },
+            places: { data: null, state: 'loading' },
+            weather: { data: null, state: 'loading' },
+          },
+        },
+      });
     });
   });
 
   describe('REMOVE_CITY', () => {
-    const action = { type: 'REMOVE_CITY', name: 'DIE' };
+    const action = { type: 'REMOVE_CITY', name: 'DIE' } as ActionType;
 
     it('does nothing if city is not in list', () => {
-      const inputState = ['FOO'];
+      const inputState = { allIds: ['FOO'], byId: {} };
       const outputState = reducer(inputState, action);
       expect(outputState).toEqual(inputState);
     });
 
     it('removes if only item in list', () => {
-      const inputState = ['DIE'];
+      const inputState = { allIds: ['DIE'], byId: {} };
       const outputState = reducer(inputState, action);
-      expect(outputState).toEqual([]);
+      expect(outputState).toEqual({ allIds: [], byId: {} });
     });
 
     it('removes from list with multiple items', () => {
-      const inputState = ['ONE', 'DIE', 'TWO'];
+      const inputState = { allIds: ['ONE', 'DIE', 'TWO'], byId: {} };
       const outputState = reducer(inputState, action);
-      expect(outputState).toEqual(['ONE', 'TWO']);
+      expect(outputState).toEqual({ allIds: ['ONE', 'TWO'], byId: {} });
     });
   });
 
   describe('MOVE_CITY_UP', () => {
-    const action = { type: 'MOVE_CITY_UP', name: 'UP' };
+    const action = { type: 'MOVE_CITY_UP', name: 'UP' } as ActionType;
 
     it('does nothing if city is not in list', () => {
-      const inputState = ['FOO'];
+      const inputState = { allIds: ['FOO'], byId: {} };
       const outputState = reducer(inputState, action);
       expect(outputState).toEqual(inputState);
     });
 
     it('does nothing if city is first item in list', () => {
-      const inputState = ['UP', 'ONE'];
+      const inputState = { allIds: ['UP', 'ONE'], byId: {} };
       const outputState = reducer(inputState, action);
       expect(outputState).toEqual(inputState);
     });
 
     it('moves city up in list of multiple items', () => {
-      const inputState = ['ONE', 'TWO', 'UP', 'THREE'];
+      const inputState = { allIds: ['ONE', 'TWO', 'UP', 'THREE'], byId: {} };
       const outputState = reducer(inputState, action);
-      expect(outputState).toEqual(['ONE', 'UP', 'TWO', 'THREE']);
+      expect(outputState).toEqual({
+        allIds: ['ONE', 'UP', 'TWO', 'THREE'],
+        byId: {},
+      });
+    });
+  });
+
+  describe('_RESPONSE, _ERROR', () => {
+    const inputState: State['cities'] = {
+      allIds: ['bar', 'foo'],
+      byId: {
+        bar: {
+          distance: {
+            data: 100,
+            state: 'ready',
+          },
+          places: {
+            data: null,
+            state: 'loading',
+          },
+          weather: {
+            data: null,
+            state: 'loading',
+          },
+        },
+        foo: {
+          distance: {
+            data: null,
+            state: 'loading',
+          },
+          places: {
+            data: null,
+            state: 'loading',
+          },
+          weather: {
+            data: null,
+            state: 'loading',
+          },
+        },
+      },
+    };
+
+    it('updates nested state on response', () => {
+      const action = {
+        name: 'foo',
+        response: 200,
+        type: 'DISTANCE_RESPONSE',
+      } as ActionType;
+
+      expect(reducer(inputState, action)).toEqual({
+        ...inputState,
+        byId: {
+          ...inputState.byId,
+          foo: {
+            ...inputState.byId.foo,
+            distance: {
+              data: 200,
+              state: 'ready',
+            },
+          },
+        },
+      });
+    });
+
+    it('updates nested state on error', () => {
+      const action = {
+        name: 'foo',
+        type: 'DISTANCE_ERROR',
+      } as ActionType;
+
+      expect(reducer(inputState, action)).toEqual({
+        ...inputState,
+        byId: {
+          ...inputState.byId,
+          foo: {
+            ...inputState.byId.foo,
+            distance: {
+              data: null,
+              state: 'error',
+            },
+          },
+        },
+      });
     });
   });
 });
